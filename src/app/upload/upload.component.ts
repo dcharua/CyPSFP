d82831b9140195e29858c484b04d64e5854bd691
halabe15 <halabe15@gmail.com>
2018-12-04 20:57:13 -0600

    algoritmo

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -34,12 +34,55 @@ export class UploadComponent implements OnInit {
       for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
       var bstr = arr.join("");
       var workbook = XLSX.read(bstr, {type:"binary"});
-      var first_sheet_name = workbook.SheetNames[0];
+      var first_sheet_name = workbook.SheetNames[1];
       var worksheet = workbook.Sheets[first_sheet_name];
-      console.log(XLSX);
-      console.log(XLSX.utils.sheet_to_json(worksheet,{raw:true}));
+      // console.log(XLSX);
+      // console.log(XLSX.utils.sheet_to_json(worksheet,{raw:true}));
+      this.check(XLSX.utils.sheet_to_json(worksheet,{raw:true}));
     }
     fileReader.readAsArrayBuffer(this.file);
   }
+
+  check(arr){
+    var empalmadas = [];
+    var actualSemester;
+    var added = false;
+    for (var i in arr) {
+      if(arr[i].Semestre == undefined)
+        continue;
+      else
+        actualSemester = arr[i].Semestre;
+
+      for (var j in arr) {
+        if(i == j) continue;
+        if(arr[j].Semestre >= actualSemester-1 && arr[j].Semestre <= actualSemester+1){
+          added = false;
+          if(arr[j]['Día 1'] == arr[i]['Día 1'] && arr[j]['Hora 1'] == arr[i]['Hora 1'] && arr[j]['Día 1'] != undefined && arr[j]['Hora 1'] != undefined){
+            for (var k in empalmadas) {
+              if(empalmadas[k].primera == arr[j] && empalmadas[k].segunda == arr[i] && empalmadas[k].dia == 1){
+                added = true;
+                break;
+              }
+            }
+            if(!added)
+              empalmadas.push({primera: arr[i], segunda: arr[j], dia:1})
+          }
+          if(arr[j]['Día 2'] == arr[i]['Día 2'] && arr[j]['Hora 2'] == arr[i]['Hora 2'] && arr[j]['Día 2'] != undefined && arr[j]['Hora 2'] != undefined){
+            for (var k in empalmadas) {
+              if(empalmadas[k].primera == arr[j] && empalmadas[k].segunda == arr[i] && empalmadas[k].dia == 2){
+                added = true;
+                break;
+              }
+            }
+            if(!added)
+              empalmadas.push({primera: arr[i], segunda: arr[j], dia:2})
+          }
+        }
+      }
+
+    }
+    console.log(empalmadas);
+  }
+
 
 }
